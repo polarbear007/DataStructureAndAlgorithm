4f222ffeb731d3306226b604b60769afe664bf39
eric <1479124685@qq.com>
2019-07-26 00:11:28 +0800

    为了方便后面获取最短路径树，我们这里另外添加一个获取 顶点索引集合的方法，逻辑基本不变，只不过添加顶点的索引

--- src/test/java/_24/cn/itcast/spt/topoLogicalSort/TopoLogicalSortForAdjacencyListWithQueue.java
+++ src/test/java/_24/cn/itcast/spt/topoLogicalSort/TopoLogicalSortForAdjacencyListWithQueue.java
@@ -72,4 +72,63 @@ public class TopoLogicalSortForAdjacencyListWithQueue<V> extends TopoLogicalSort
 		}
 		return list;
 	}
+	
+	/**
+	 * 	跟 topoLogicalSort 功能差不多，不过返回的是索引值集合
+	 * @return
+	 */
+	public List<Integer> topoLogicalSortForIndexList() {
+		// 首先，我们需要创建返回的 List 对象
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		// 然后，我们需要遍历有向图全部的边，初始化 indegree 数组
+		initIndegree();
+		// 初始化 visited 数组
+		initVisited();
+		Vertex<V>[] adjTable = graph.getAdjTable();
+		
+		// 创建一个队列
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		// 创建一个一维数组 isInQueue ，记录一个顶点是否在队列中
+		boolean[] isInQueue = new boolean[graph.getVertexCount()];
+		
+		// 把所有入度为0 的顶点索引全部添加到队列中
+		for (int i = 0; i < indegree.length; i++) {
+			if(indegree[i] == 0) {
+				queue.add(i);
+				isInQueue[i] = true;
+			}
+		}
+		
+		Integer currIndex = -1;
+		LinkedList<Edge> adjList = null;
+		int toIndex = -1;
+		
+		while(!queue.isEmpty()) {
+			currIndex = queue.poll();
+			// 维护 isInQueue 数组
+			isInQueue[currIndex] = false;
+			// 把这个顶点设置成已访问 
+			visited[currIndex] = true;
+			// 把这个顶点的索引添加到 list 集合中
+			list.add(currIndex);
+			
+			// 再然后，我们去获取这个顶点相邻的顶点
+			adjList = adjTable[currIndex].getAdjList();
+			for (Edge edge : adjList) {
+				toIndex = edge.getToIndex();
+				// 不管怎么样，我们先维护 indegree 
+				indegree[toIndex]--;
+				// 如果相邻的顶点入度为 0 ,并且不在队列中，那么我们就 添加到队列
+				if(indegree[toIndex] == 0 && !isInQueue[toIndex]) {
+					queue.add(toIndex);
+				}
+			}
+		}
+		
+		
+		if(list.size() < adjTable.length) {
+			throw new RuntimeException("此有向图存在环，不可进行拓朴排序");
+		}
+		return list;
+	}
 }
--- src/test/java/_24/cn/itcast/spt/topoLogicalSort/TopoLogicalSortForAdjacencyMatrixWithQueue.java
+++ src/test/java/_24/cn/itcast/spt/topoLogicalSort/TopoLogicalSortForAdjacencyMatrixWithQueue.java
@@ -73,4 +73,62 @@ public class TopoLogicalSortForAdjacencyMatrixWithQueue<V> extends TopoLogicalSo
 		}
 		return list;
 	}
+	
+	public List<Integer> topoLogicalSortForIndexList() {
+		// 首先，我们需要创建返回的 List 对象
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		// 然后，我们需要遍历有向图全部的边，初始化 indegree 数组
+		initIndegree();
+		// 初始化 visited 数组
+		initVisited();
+
+		Double[][] adjMatrix = graph.getAdjMatrix();
+		ArrayList<V> vertexs = graph.getVertexs();
+		
+		// 我们再维护一个队列，来优化排序的过程
+		LinkedList<Integer> queue = new LinkedList<>();
+		// 为了防止一个顶点重复入队列，我们再维护一个  isInQueue 数组
+		boolean[] isInQueue = new boolean[graph.getVertexCount()];
+		
+		// 首先，我们需要遍历 indegree 数组，找到全部入度为 0 的顶点，全部添加到队列中
+		for (int i = 0; i < indegree.length; i++) {
+			if(indegree[i] == 0) {
+				queue.add(i);
+				isInQueue[i] = true;
+			}
+		}
+		
+		Integer currIndex = -1;
+		// 再然后，就是不断地从队列中弹出顶点，并处理的过程
+		while(!queue.isEmpty()) {
+			currIndex = queue.poll();
+			// 首先，我们把这个顶点标记成不在队列中
+			isInQueue[currIndex] = false;
+			// 然后，我们需要把这个顶点标记成已访问 
+			visited[currIndex] = true;
+			// 再然后，我们需要把这个顶点添加到 list 集合中
+			list.add(currIndex);
+			
+			// 再然后，我们需要根据 邻接矩阵，维护邻接顶点的入度
+			for (int i = 0; i < adjMatrix[currIndex].length; i++) {
+				// 我们只处理 未访问过的顶点
+				if(!visited[i] && adjMatrix[currIndex][i] != null) {
+					// 不管怎么样，先维护 indegree 数组
+					indegree[i]--;
+					// 是否放进队列有两个条件： 
+					//  1、 没有在队列中，我们不重复添加
+					//  2、 入度为 0 ;
+					if(indegree[i] == 0 && !isInQueue[i]) {
+						queue.add(i);
+					}
+				}
+			}
+		}
+
+		// 前面循环处理结束以后，我们再遍历一下 visited 数组，如果发现还有 false 的，说明还是存在环，直接抛异常
+		if(list.size() < graph.getVertexCount()) {
+			throw new RuntimeException("此图含有环，无法进行拓朴排序");
+		}
+		return list;
+	}
 }
